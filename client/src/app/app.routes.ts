4ebb3b189bf3c7446ffb67a9a49a1c16a93ea65c
Namrata Kumari <namrata.kumari@argusoft.com>
2026-01-22 15:56:15 +0530

    home page after login

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,23 +1,28 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './auth/register/register.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    {
-        path:'',
-        redirectTo:'register',
-        pathMatch:'full'
-    },
-    {
-        path:'register',
-        component:RegisterComponent
-    },
-    {
-        path:'login',
-        loadComponent:()=>
-            import('./auth/login/login.component').then(m=>m.LoginComponent)
-    },
-    {
-        path:'**',
-        redirectTo:'register'
-    }
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./auth/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'register',
+  },
 ];
