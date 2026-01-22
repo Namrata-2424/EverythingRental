9c3e1b27a6ede65cbdc65321e9b0fe2b9267547d
Namrata Kumari <namrata.kumari@argusoft.com>
2026-01-22 12:08:25 +0530

    angular 17 re-initialized

--- client/src/app/auth/register/register.component.ts
+++ client/src/app/auth/register/register.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
 
